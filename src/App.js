303d99ea975232cae3ab7311060c206ab955b81a
gokhanipek <gipek03@gmail.com>
2022-03-20 20:25:50 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import * as THREE from 'three'
+
+import React, { useRef, useMemo, useState, Suspense } from 'react'
+import { Canvas, createPortal, useFrame, useThree } from '@react-three/fiber'
+import { PerspectiveCamera, OrbitControls, TorusKnot, Box, Text, Html } from '@react-three/drei'
+
 import './App.css';
 
-function App() {
+function TextShadowScene() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <React.Suspense fallback={null}>
+          <Text
+          color={'#EC2D2D'}
+          fontSize={12}
+          maxWidth={200}
+          lineHeight={1}
+          letterSpacing={0.02}
+          textAlign={'left'}
+          font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+          anchorX="center"
+          anchorY="middle"
+          outlineOffsetX={'10%'}
+          outlineOffsetY={'10%'}
+          outlineBlur={'30%'}
+          outlineOpacity={0.3}
+          outlineColor="#EC2D2D"
+          >
+          LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
+          MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
+          CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA
+          PARIATUR. EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST
+          LABORUM.
+          </Text>
+      </React.Suspense>
+  )
+  }
+
+
+function Cube() {
+  const cam = useRef();
+  const myMesh = useRef();
+  const boxRef = useRef();
+  const [cubeRot, setCubeRot] = useState(Math.PI * 0)
+
+  useFrame(() => {
+    boxRef.current.rotation.y += Math.PI * 0.0;
+  });
+
+
+  const [scene, target] = useMemo(() => {
+    const scene = new THREE.Scene()
+    scene.background = new THREE.Color('orange')
+    const target = new THREE.WebGLMultisampleRenderTarget(1024, 1024, {
+      format: THREE.RGBFormat,
+      stencilBuffer: false
+    })
+    target.samples = 8
+    return [scene, target]
+  }, [])
+
+  useFrame((state) => {
+    cam.current.position.z = 5 + Math.sin(state.clock.getElapsedTime() * 1.5) * 2
+    state.gl.setRenderTarget(target)
+    state.gl.render(scene, cam.current)
+    state.gl.setRenderTarget(null)
+  })
+
+  const onClickHandler = () => {
+    console.warn(myMesh.current);
+  }
+
+
+  return (
+    <>
+      <PerspectiveCamera ref={cam} position={[1, 1, 1]} />
+      <Box ref={boxRef} rotation-y={0} args={[2, 2, 2]} onClick={onClickHandler} >
+        <meshStandardMaterial attach="material" map={target.texture} />
+        <TextShadowScene />
+      </Box>
+    </>
+  )
 }
 
-export default App;
+
+export default Cube;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import React, { useRef, useMemo } from 'react'
+import { Canvas, createPortal, useFrame } from '@react-three/fiber'
+import { PerspectiveCamera, OrbitControls, TorusKnot, Box, Text, Icosahedron, Html } from '@react-three/drei'
+import Cube from './App'
+
+function TextShadowScene() {
+  
+  return (
+      <React.Suspense fallback={null}>
+          <Text
+          color={'#EC2D2D'}
+          fontSize={12}
+          maxWidth={200}
+          lineHeight={1}
+          letterSpacing={0.02}
+          textAlign={'left'}
+          font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+          anchorX="center"
+          anchorY="middle"
+          outlineOffsetX={'10%'}
+          outlineOffsetY={'10%'}
+          outlineBlur={'30%'}
+          outlineOpacity={0.3}
+          outlineColor="#EC2D2D"
+          >
+          LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
+          MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
+          CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA
+          PARIATUR. EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST
+          LABORUM.
+          </Text>
+      </React.Suspense>
+  )
+  }
+
+const NUM = 3;
+function PerspectiveCameraScene() {
+  const positions = React.useMemo(() => {
+    const pos = []
+    const half = (NUM - 1) / 2
+
+    for (let x = 0; x < NUM; x++) {
+      for (let y = 0; y < NUM; y++) {
+        pos.push({
+          id: `${x}-${y}`,
+          position: [(x - half) * 4, (y - half) * 4, 0],
+        })
+      }
+    }
+
+    return pos
+  }, [])
+
+  const items = [
+    "Hi my name is gokhan and I am the real msaaadadsdsfc",
+    "Hi my name is gokhan and I am the realsdfsfsfsdfsd mc",
+    "Hi my name is gokhan and I am the real hghhfhffmc",
+    "Hi my name is gokhan and I am saddasdasdthe real mc",
+  ]
+
+  return (
+    <>
+      <PerspectiveCamera makeDefault position={[0, 0, 10]} />
+      <group position={[0, 0, -10]}>
+        {positions.map(({ id, position }) => (
+          <Icosahedron key={id} position={position} args={[1, 1]}>
+            <meshBasicMaterial attach="material" color="white" />
+            <Html position={[0, 0, -6]} className="html-story-label">
+            {items[id]}
+            {id}
+          </Html>
+          </Icosahedron>
+        ))}
+      </group>
+      <OrbitControls />
+    </>
+  )
+}
+
+export const PerspectiveCameraSceneSt = () => <PerspectiveCameraScene />
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Canvas colorManagement>
+    <ambientLight />
+    <spotLight position={[10, 10, 10]} />
+    <pointLight position={[-10, -10, 0]} color="black" />
+    <PerspectiveCameraScene />
+    <OrbitControls />
+  </Canvas>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
